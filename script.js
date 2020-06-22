a13f078a3ce1b4999d4d1d8b8865124b44781892
Himanshu Sharma <himanshu.sharma713@gmail.com>
2020-06-22 21:14:53 +0530

    added javascript file and some styles

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("twitterButton").onclick=function(){
+    document.getElementById("twitterButton").innerHTML = "himanshu143nith";
+}
+
+document.getElementById("githubButton").onclick=function(){
+    document.getElementById("githubButton").innerHTML = "himanshu143nith";
+}
+document.getElementById("kaggleButton").onclick=function(){
+    document.getElementById("kaggleButton").innerHTML = "himanshusharma713";
+}
