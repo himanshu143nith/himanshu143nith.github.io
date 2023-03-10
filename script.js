e9d78ae88b64d80b8efc6917ee6642080fdd9028
Himanshu Sharma <himanshu143nith@users.noreply.github.com>
2023-03-10 06:08:40 +0000

    added links to profile and same content in index file

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.getElementById("twitterButton").onclick=function(){
-    document.getElementById("twitterButton").innerHTML = "himanshu143nith";
+    document.getElementById("twitterButton").innerHTML = "https://twitter.com/Himanshu143nith";
 }
 
 document.getElementById("githubButton").onclick=function(){
-    document.getElementById("githubButton").innerHTML = "himanshu143nith";
+    document.getElementById("githubButton").innerHTML = "https://github.com/himanshu143nith";
 }
 document.getElementById("kaggleButton").onclick=function(){
-    document.getElementById("kaggleButton").innerHTML = "himanshusharma713";
+    document.getElementById("kaggleButton").innerHTML = "https://www.kaggle.com/himanshusharma713";
 }
